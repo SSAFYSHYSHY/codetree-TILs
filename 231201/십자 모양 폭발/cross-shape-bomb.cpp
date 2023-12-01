204777111913b7b899da9ba3bc4b9885d712375c
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2023-12-01 19:56:30 +0900

    Title: 십자 모양 폭발, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231201/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[201][201];
+int temp[201][201];
+int n;
+int r, c;
+
+bool InRange(int x) {
+	return (1 <= x && x <= n);
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	cin >> r >> c;
+
+	int a = arr[r][c];
+	a -= 1;
+
+	for (int i = r - a; i <= r + a; i++) {
+		if (InRange(i)) {
+			arr[i][c] = 0;
+		}
+	}
+	for (int i = c - a; i <= c + a; i++) {
+		if (InRange(i)) {
+			arr[r][i] = 0;
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int cnt = 4;
+		for (int j = n; j >= 1; j--) {
+			if (arr[j][i] != 0) {
+				temp[cnt][i] = arr[j][i];
+				cnt--;
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			arr[i][j] = temp[i][j];
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+}
