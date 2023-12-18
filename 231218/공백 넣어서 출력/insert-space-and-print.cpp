75847679e8e97a00dac8d0178df1d9c1da64c96d
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2023-12-18 18:51:46 +0900

    Title: 공백 넣어서 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231218/공백 넣어서 출력/insert-space-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "score          subject          name\n";
+    cout <<"\n";
+    cout <<"  100             math           sam\n";
+    cout <<"   88          science          paul\n";
+    cout <<"   96         language          jack";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
