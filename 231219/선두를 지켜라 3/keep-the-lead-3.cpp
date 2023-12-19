f2e466ac8653f11c929189a27976c7150e6b6bd7
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2023-12-19 19:59:18 +0900

    Title: 선두를 지켜라 3, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231219/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+int arr[1001], brr[1001];
+int cnt = 0, flag = 0;
+
+int main() {
+	cin >> a >> b;
+
+	int a_num = 1;
+	for (int i = 0; i < a; i++) {
+		int v, t;
+
+		cin >> v >> t;
+
+		while (t--) {
+			arr[a_num] = arr[a_num - 1] + v;
+			a_num++;
+		}
+	}
+
+	int b_num = 1;
+	for (int i = 0; i < b; i++) {
+		int v, t;
+
+		cin >> v >> t;
+
+		while (t--) {
+			brr[b_num] = brr[b_num - 1] + v;
+			b_num++;
+		}
+	}
+
+	for (int i = 0; i < a_num; i++) {
+		if (arr[i] > brr[i]) {
+			if (flag == 2 || flag == 3) {
+				cnt++;
+			}
+
+			flag = 1;
+		}
+		else if (arr[i] < brr[i]) {
+			if (flag == 1 || flag == 3) {
+				cnt++;
+			}
+
+			flag = 2;
+		}
+		else if (arr[i] == brr[i]) {
+			if (flag == 2 || flag == 1) {
+				cnt++;
+			}
+
+			flag = 3;
+		}
+	}
+
+	cout << cnt;
+}
