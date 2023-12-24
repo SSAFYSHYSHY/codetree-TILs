f94688cc0d987af812e60b188043175eed81a1e2
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2023-12-24 14:28:22 +0900

    Title: 야바위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231224/야바위/ya-rock.cpp
+++ 231224/야바위/ya-rock.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int N;
-int arr[3] = {0,};
+int arr[4] = {0,};
 int a[101], b[101], c[101];
 
 int main() {
@@ -15,8 +15,9 @@ int main() {
 	}
 
 	int ans = 0;
-	for (int i = 0; i < 3; i++) {
+	for (int i = 1; i <= 3; i++) {
 		int cnt = 0;
+		arr[1] = arr[2] = arr[3] = 0;
 		arr[i] = 1;
 
 		for (int j = 0; j < N; j++) {
@@ -25,9 +26,9 @@ int main() {
 			if (arr[c[j]] == 1) {
 				cnt++;
 			}
-		}
 
-		arr[i] = 0;
+			//cout << arr[a_n] << " " << arr[b_n] << " " << arr[c_n] << "\n";
+		}
 		ans = max(ans, cnt);
 	}
 	cout << ans;
