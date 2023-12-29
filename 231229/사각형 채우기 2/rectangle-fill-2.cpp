4ffb4cd60bea8c511d3839ac958dfc1b612c550f
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2023-12-29 13:55:56 +0900

    Title: 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/사각형 채우기 2/rectangle-fill-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[1001];
+
+int main() {
+	arr[1] = 1;
+	arr[2] = 3;
+
+	cin >> n;
+
+	for (int i = 3; i <= n; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2]*2)%10007;
+	}
+	cout << arr[n];
+}
