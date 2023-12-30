91aeecf81392516037109fcf7baf7f304926bada
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2023-12-30 21:35:03 +0900

    Title: 달에서 무게 구하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231230/달에서 무게 구하기 2/weight-on-the-moon-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(5);
+
+    int a = 147;
+    double b = 0.165;
+
+    cout << a << " * " << b << " = " << a * b;
+
+
+
+    return 0;
+}
