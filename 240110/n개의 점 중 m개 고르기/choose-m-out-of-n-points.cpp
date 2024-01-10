daa79ab8529d6830e2cb01aa3bd95ba9b5cb72ce
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-10 20:01:59 +0900

    Title: n개의 점 중 m개 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <cmath>
+
+using namespace std;
+
+pair<int, int> p[21];
+int n, m;
+double ans = 21e8;
+vector<pair<int, int>> v;
+
+double Calc2(int num) {
+	double diff = 0.0;
+
+	for (int i = 0; i < num; i++) {
+		int x, y, x1, y1;
+		tie(x, y) = v[i];
+		for (int j = i + 1; j < num; j++) {
+			tie(x1, y1) = v[j];
+			diff = max((double)sqrt(pow((x1 - x), 2) + pow((y1 - y), 2)), diff);
+		}
+	}
+
+	return diff;
+}
+
+void Calc(int curr, int idx) {
+	if (curr == m  ) {
+		ans = min(ans, Calc2(m));
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		v.push_back(p[i]);
+		Calc(curr + 1, i + 1);
+		v.pop_back();
+	}
+}
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		p[i] = { x,y };
+	}
+
+	Calc(0, 0);
+
+	cout << (int)pow(ans,2);
+}
