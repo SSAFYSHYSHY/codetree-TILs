336a7838067ee3a7108000719557139536040675
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-12 20:15:08 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240112/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
+int n;
+int arr[1001] = {0,};
+int dp[1001] = {0,};
+
 int main() {
-    int num;
-    cin >> num;
+	cin >> n;
 
-    vector<int> a(num + 1);
-    vector<int> dp(num + 1, 0);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+        dp[i] = 1;
+	}
 
-    for (int i = 1; i <= num; ++i) {
-        cin >> a[i];
-    }
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
 
-    dp[1] = 1;
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans = max(ans, dp[i]);
+	}
 
-    for (int i = 2; i <= num; ++i) {
-        for (int j = 1; j < i; ++j) {
-            if (a[j] < a[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-    }
-
-    int result = 0;
-    for (int i = 1; i <= num; ++i) {
-        result = max(result, dp[i]);
-    }
-
-    cout << result << endl;
-
-    return 0;
+	cout << ans;
 }
