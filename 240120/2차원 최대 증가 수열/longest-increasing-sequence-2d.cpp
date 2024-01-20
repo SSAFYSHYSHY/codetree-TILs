afbbf5b1a71624c5a603e0a3a6a87548fef01911
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-20 16:36:55 +0900

    Title: 2차원 최대 증가 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240120/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 240120/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -1,44 +1,55 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
+
+#define MAX_NUM 50
 
 using namespace std;
 
-int arr[51][51];
-int dp[51][51];
+// 변수 선언
 int n, m;
+int arr[MAX_NUM][MAX_NUM];
 
-void Initial() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-			dp[i][j] = 1;
-		}
-	}
+int dp[MAX_NUM][MAX_NUM];
+
+void Initialize() {
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            dp[i][j] = INT_MIN;
+    
+
+    dp[0][0] = 1;
 }
 
 int main() {
-	cin >> n >> m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> arr[i][j];
 
-	Initial();
+    Initialize();
 
-	for (int x = 1; x < n; x++) {
-		for (int y = 1; y < m; y++) {
-			for (int i = 0; i < x; i++) {
-				for (int j = 0; j < y; j++) {
-					if (arr[x][y] > arr[i][j]) {
-						dp[x][y] = max(dp[x][y], dp[i][j] + 1);
-					}
-				}
-			}
-		}
-	}
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++) {
 
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			ans = max(ans, dp[i][j]);
-		}
-	}
+            for(int k = 0; k < i; k++)
+                for(int l = 0; l < j; l++) {
 
-	cout << ans;
+                    if(dp[k][l] == INT_MIN)
+                        continue;
+
+
+                    if(arr[k][l] < arr[i][j])
+                        dp[i][j] = max(dp[i][j], dp[k][l] + 1);
+                }
+        }
+
+    int ans = INT_MIN;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            ans = max(ans, dp[i][j]);
+    
+    cout << ans;
+    return 0;
 }
