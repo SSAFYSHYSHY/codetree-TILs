c7a3037562e00f0eae33ad27bb295041fe96d908
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-20 15:42:08 +0900

    Title: 최대 감소 부분 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[1001];
+int dp[1001];
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		//cout << dp[i] << " ";
+
+		ans = max(dp[i], ans);
+	}
+	cout << ans;
+
+}
