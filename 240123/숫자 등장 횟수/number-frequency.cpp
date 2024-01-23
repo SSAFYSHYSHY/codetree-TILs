06acfab0c39c935c4a7c52368221882a31e1a54b
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-23 19:40:42 +0900

    Title: 숫자 등장 횟수, Time: 91ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240123/숫자 등장 횟수/number-frequency.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+unordered_map<int, int> v;
+int n, m;
+int arr[100001];
+vector<int> v1;
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+
+		v[arr[i]] = i+1;
+	}
+
+	for (int i = 0; i < m; i++) {
+		int a;
+		cin >> a;
+		v1.push_back(v[a]);
+	}
+
+	for (int i = 0; i < v1.size(); i++) {
+		cout << v1[i] << " ";
+	}
+}
