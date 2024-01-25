dd951ff31374e97711f7b1fbcee04e304a20e59f
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-25 19:20:36 +0900

    Title: 고대 보물 지도의 비밀, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240125/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+int arr[100005];
+int dp[100005][15];
+
+int main() {
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+
+	int ans = INT_MIN;
+
+	for (int i = 1; i <= n; i++) {
+		if (arr[i] >= 0) {
+			for (int j = 0; j <= k; j++) {
+				dp[i][j] = max(dp[i - 1][j] + arr[i], dp[i][j]);
+				ans = max(ans, dp[i][j]);
+			}
+		}
+		else {
+			for (int j = 1; j <= k; j++) {
+				dp[i][j] = max(dp[i - 1][j - 1] + arr[i], dp[i][j]);
+				ans = max(ans, dp[i][j]);
+			}
+		}
+	}
+
+	cout << ans;
+}
