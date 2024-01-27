c650b41a489d0086cd68b65ef81030609286c92f
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-27 19:27:13 +0900

    Title: 둘 중 하나 잘 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/둘 중 하나 잘 고르기/choose-one-of-two-points.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int arr[202];
+int brr[202];
+int n;
+int dp[202][101];
+bool visited[202][101];
+
+int Calc(int idx, int num) {
+	if (idx == 0 && num > 0 || num < 0) return INT_MIN;
+
+	if (idx == 0) {
+		return 0;
+	}
+
+	if (visited[idx][num]) {
+		return dp[idx][num];
+	}
+
+	visited[idx][num] = true;
+
+	dp[idx][num] = max(Calc(idx - 1, num) + brr[idx], Calc(idx - 1, num - 1) + arr[idx]);
+
+	return dp[idx][num];
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 1; i <= 2*n; i++) {
+		cin >> arr[i] >> brr[i];
+	}
+
+	for (int i = 0; i <= 2*n; i++) {
+		for (int j = 0; j <= n; j++) {
+			dp[i][j] = INT_MIN;
+		}
+	}
+
+	int ans = Calc( n, n);
+
+	cout << ans;
+}
