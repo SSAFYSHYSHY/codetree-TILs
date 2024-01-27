26c3ed9d59e84189594a30904b12533667902c2a
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-27 19:35:03 +0900

    Title: M개의 구간을 선택하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/M개의 구간을 선택하기/select-m-sections.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+enum State {
+	NOT_b,
+	b
+};
+
+int n, m;
+int arr[501];
+int dp[501][251][2];
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			dp[i][j][NOT_b] = dp[i][j][b] = INT_MIN;
+		}
+	}
+
+	for (int i = 0; i <= n; i++) {
+		dp[i][0][NOT_b] = 0;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			dp[i][j][b] = max(dp[i - 1][j - 1][NOT_b] + arr[i], dp[i - 1][j][b] + arr[i]);
+
+			dp[i][j][NOT_b] = max(dp[i - 1][j][NOT_b], dp[i - 1][j][b]);
+		}
+	}
+
+	cout << max(dp[n][m][NOT_b], dp[n][m][b]);
+}
