201acd9412abd19cb6908149a8b982a1c3b8f410
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-28 18:25:24 +0900

    Title: 앞에서부터 삭제하기 2, Time: 35ms, Memory: 1MB, Status: Passed - CodeTree

--- 240128/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 240128/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -4,40 +4,30 @@
 
 using namespace std;
 
-int n;
+int n, sum = 0;
 int arr[100001];
-double ans;
+double ans,avg;
+priority_queue<int>q;
 
 int main() {
 	cout << fixed;
 	cout.precision(2);
 
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
 
-	for (int k = 0; k < n - 2; k++) {
+    
+    sum += arr[n];
+    q.push(-arr[n]);
 
-		priority_queue<int, vector<int>, greater<int>> q;
-		int len = 0;
-		double sum = 0;
+	for (int k = n-2; k >= 1 ; k--) {
+        sum += arr[k + 1];
+        q.push(-arr[k + 1]);
 
-		for (int j = k  +  1 ; j < n; j++) {
-			q.push(arr[j]);
-		}
-
-		q.pop();
-		len = q.size();
-
-		while (!q.empty()) {
-			int a = q.top();
-			sum += a;
-			q.pop();
-		}
-		sum = sum / len;
-
-		ans = max(ans, (double)sum);
+		double avg = (double)(sum - (-q.top())) / (n - k - 1);
+		ans = max(ans, avg);
 
 	}
 
