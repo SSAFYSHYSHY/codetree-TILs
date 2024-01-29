39d6427dfa03310f4d59d56f4768ce5b3dbfa426
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-01-29 20:16:22 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+const int MAXN = 1005;
+int n;
+int dp[MAXN][5][5];
+
+int main() {
+    cin >> n;
+
+    dp[1][1][0] = 1;  // 첫 번째 날에 T를 받은 경우
+    dp[1][0][1] = 1;  // 첫 번째 날에 B를 받은 경우
+    dp[1][0][0] = 1;  // 첫 번째 날에 G를 받은 경우
+
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < 3; j++) {
+            for(int k = 0; k < 3; k++) {
+                dp[i + 1][j + 1][0] = (dp[i + 1][j + 1][0] + dp[i][j][k]) % MOD;
+                dp[i + 1][j][0] = (dp[i + 1][j][0] + dp[i][j][k]) % MOD;
+                dp[i + 1][j][k + 1] = (dp[i + 1][j][k + 1] + dp[i][j][k]) % MOD;
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int j = 0; j < 3; j++) {
+        for(int k = 0; k < 3; k++) {
+            ans = (ans + dp[n][j][k]);
+        }
+    }
+
+    cout << ans;
+}
