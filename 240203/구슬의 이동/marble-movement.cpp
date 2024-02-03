a488bbb832a9188e017d06118a1a4ef1f25f7404
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-03 16:59:07 +0900

    Title: 구슬의 이동, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/구슬의 이동/marble-movement.cpp
+++ 240203/구슬의 이동/marble-movement.cpp
@@ -1,174 +1,116 @@
 #include <iostream>
-#include <tuple>
 #include <algorithm>
 #include <vector>
+#include <tuple>
+
+#define MAX_N 50
+#define ASCII_NUM 128
+#define DIR_NUM 4
 
 using namespace std;
 
-int arr_cnt[51][51];
-typedef tuple<int, int, int, int> ball;
-
-// U R L D
-int dx[] = {-1,0,0,1};
-int dy[] = {0,1,-1,0};
-int dirc[128];
-
-vector<ball> temp_v;
-vector<ball> v;
-
 int n, m, t, k;
+vector<tuple<int, int, int> > grid[MAX_N][MAX_N];
+vector<tuple<int, int, int> > next_grid[MAX_N][MAX_N];
 
 bool InRange(int x, int y) {
-	return 1 <= x && x <= n && 1 <= y && y <= n;
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-ball Move(ball b) {
-	int x, y, dir, velo;
-	tie(x, y, dir, velo) = b;
-
-	//velo 만큼 이동.
-	for (int i = 0; i < velo; i++) {
-		//가고자 하는 방향.
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-
-		//벽이 있는지 판단. 없으면 앞으로.
-		if(InRange(nx, ny)) {
-			x = nx;
-			y = ny;
-		}
-		else {
-			//0,3 1,2 대칭 이동.
-			dir = 3 - dir;
-			i--;
-		}
-	}
-
-	return make_tuple(x, y, dir, velo);
+tuple<int, int, int> NextPos(int x, int y, int vnum, int move_dir) {
+	int dx[DIR_NUM] = {-1, 0, 0, 1};
+	int dy[DIR_NUM] = {0, 1, -1, 0};
+    
+    // vnum 횟수만큼 이동한 이후의 위치를 반환합니다.
+    while(vnum--) {
+        int nx = x + dx[move_dir], ny = y + dy[move_dir];
+        // 벽에 부딪히면
+        // 방향을 바꾼 뒤 이동합니다.
+        if(!InRange(nx, ny)) {
+            move_dir = 3 - move_dir;
+            nx = x + dx[move_dir]; ny = y + dy[move_dir];
+        }
+        x = nx; y = ny;
+    }
+    return make_tuple(x, y, move_dir);
 }
 
 void MoveAll() {
-	for (int i = 0; i < v.size(); i++) {
-		v[i] = Move(v[i]);
-	}
+    for(int x = 0; x < n; x++)
+        for(int y = 0; y < n; y++)
+            for(int i = 0; i < (int) grid[x][y].size(); i++) {
+                int v, num, move_dir;
+                tie(v, num, move_dir) = grid[x][y][i];
+                
+                int next_x, next_y, next_dir;
+                // v값이 음수이므로, -를 붙여 넘겨줍니다.
+                tie(next_x, next_y, next_dir) = NextPos(x, y, -v, move_dir);
+                next_grid[next_x][next_y].push_back(
+                    make_tuple(v, num, next_dir)
+                );
+            }
 }
 
-//해당 구슬과 충돌이 일어난 구슬이 있는지 확인. k 개 미만인지 확인.
-bool Calc2(int target_idx) {
-	int tar_x, tar_y;
-	tie(tar_x, tar_y, ignore, ignore) = v[target_idx];
-
-	return arr_cnt[tar_x][tar_y] >= k;
+void SelectMarbles() {
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) 
+            if((int) next_grid[i][j].size() >= k) {
+                // 우선순위가 높은 k개만 남겨줍니다.
+                sort(next_grid[i][j].begin(), next_grid[i][j].end());
+                while((int) next_grid[i][j].size() > k)
+                    next_grid[i][j].pop_back();
+            }
 }
 
-///// 1. 우선순위에 따른 구슬의 생존, 나머지는 삭제.
-///////1.1 구슬의 속도가 가장 빠른 것부터 우선순위가 높음.
-///////1.2 구슬의 속도가 일치하면, 구슬의 번호가 더 큰 구슬의 우선순위가 높음.
-
-void Calc3(int x, int y) {
-	bool flag[62] = { false, };
-	//k 만큼 반복, 우선 순위를 찾아 새로 삽입.
-	for (int i = 0; i < k; i++) {
-		int ans = 0;
-		int idx = 0;
-
-		//해당 위치한 배열들.
-		for (int j = 0; j < v.size(); j++) {
-			int x, y, velo;
-			tie(x, y, ignore, velo) = v[j];
-
-			//해당 벡터가 일치하는 좌표이고 방문도 하지 않았어야 함..
-			if (x == x && y == y && flag[j] == false) {
-				if (velo > ans) {
-					ans = velo;
-					idx = j;
-				}
-				else if (velo >= ans && j > idx) {
-					ans = velo;
-					idx = j;
-				}
-				temp_v.push_back(v[j]);
-				flag[j] = true;
-				break;
-			}
-			else continue;
-		}
-	}
-}
-
-//충돌이 일어난 구슬.
-void Calc() {
-	temp_v.clear();
-
-	//각 위치에 대한 수 증가.
-	for (int i = 0; i < v.size(); i++) {
-		int x, y;
-		tie(x, y, ignore, ignore) = v[i];
-
-		arr_cnt[x][y]++;
-	}
-
-	//충돌에 대한 구슬 기록.
-	for (int i = 0; i < v.size(); i++) {
-		//충돌이 일어나지 않은 구역.
-		if (!Calc2(i)) {
-			temp_v.push_back(v[i]);
-		}
-		//충돌이 일어난 구역.
-		///// 1. 우선순위에 따른 구슬의 생존, 나머지는 삭제.
-		///////1.1 구슬의 속도가 가장 빠른 것부터 우선순위가 높음.
-		///////1.2 구슬의 속도가 일치하면, 구슬의 번호가 더 큰 구슬의 우선순위가 높음.
-		else {
-			//해당 배열칸을 연산.
-			int tar_x, tar_y;
-			tie(tar_x, tar_y, ignore, ignore) = v[i];
-
-			Calc3(tar_x, tar_y);
-		}
-	}
-
-	//각 구슬의 위치가 적어놓은 count 수를 초기화.
-	for (int i = 0; i < v.size(); i++) {
-		int x, y;
-		tie(x, y, ignore, ignore) = v[i];
-
-		arr_cnt[x][y]--;
-	}
-
-	//연산한 것들을 다시 채워줌.
-	v = temp_v;
-}
-
-void Simul() {
-	//v 크기 만큼 Move() 함수.
-	MoveAll();
-
-
-	//충돌이 일어나는 구슬에 대한 연산
-	Calc();
+void Simulate() {
+    // Step1. next_grid를 초기화합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            next_grid[i][j].clear();
+		
+    // Step2. 구슬들을 전부 움직입니다.
+    MoveAll();
+    
+    // Step3. 각 칸마다 구슬이 최대 k개만 있도록 조정합니다.
+    SelectMarbles();
+    
+    // Step4. next_grid 값을 grid로 옮겨줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            grid[i][j] = next_grid[i][j];
 }
 
 int main() {
 	cin >> n >> m >> t >> k;
-
-	dirc['U'] = 0;
-	dirc['R'] = 1;
-	dirc['L'] = 2;
-	dirc['D'] = 3;
-
-	for (int i = 0; i < m; i++) {
-		int x, y, velo;
-		char d;
-
-		cin >> x >> y >> d >> velo;
-
-		v.push_back(make_tuple(x,y, dirc[d], velo));
+	
+	int dir_mapper[ASCII_NUM];
+	dir_mapper['U'] = 0;
+	dir_mapper['R'] = 1;
+	dir_mapper['L'] = 2;
+	dir_mapper['D'] = 3;
+	
+	for(int i = 0; i < m; i++) {
+		int r, c, v; char d;
+		cin >> r >> c >> d >> v;
+        
+        // 살아남는 구슬의 우선순위가 더 빠른 속도, 더 큰 번호 이므로
+        // 정렬시 속도가 먼저 내림차순, 그 다음에는 번호가 내림차순으로 오도록
+        // (-속도, -번호, 방향) 순서를 유지합니다.
+		grid[r - 1][c - 1].push_back(
+            make_tuple(-v, -(i + 1), dir_mapper[d])
+        );
 	}
-
-	//t 만큼 시뮬을 반복.
-	while (t--) {
-		Simul();
-	}
-
-	cout << v.size() << "\n";
+	
+    // t초에 걸쳐 시뮬레이션을 반복합니다.
+	while(t--)
+		Simulate();
+	
+	int ans = 0;
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			ans += (int) grid[i][j].size();
+	
+	cout << ans;
+	
+	return 0;
 }
