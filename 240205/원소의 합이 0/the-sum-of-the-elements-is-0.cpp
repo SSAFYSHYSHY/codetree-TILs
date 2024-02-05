cceaffb44638eeffca6147fa6248b907ae6cdf72
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-05 13:27:17 +0900

    Title: 원소의 합이 0, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240205/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
+++ 240205/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 #include <unordered_map>
-#include <vector>
 
 using namespace std;
 
 int n, ans = 0;
-int arr[4][5001];
-
+int A[5001], B[5001], C[5001], D[5001];
 unordered_map<int, int> v;
-unordered_map<int, int> v1;
 
 int main() {
 	cin >> n;
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> B[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> C[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> D[i];
+	}
+
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> arr[i][j];
+			v[A[i] + B[j]]++;
 		}
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			//1 + 2
-			v[arr[0][i] + arr[1][j]]++;
-
-			//3 + 4
-			v1[arr[2][i] + arr[3][j]]++;
+			int sub = -C[i] - D[j];
+			if (v.count(sub) > 0) {
+				ans += v[sub];
+			}
 		}
 	}
 
-	for (unordered_map<int, int>::iterator it = v.begin(); it != v.end(); it++) {
-		int sub = -(it->first);
-		if (v1.find(sub) != v1.end()) {
-			ans += v1[sub] * (it->second);
-		}
-	}
-
-	cout << ans << "\n";
-
+	cout << ans;
 }
