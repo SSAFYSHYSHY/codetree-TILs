f459a39ae5d4101f0f18d9e2ee7b18665b3eac32
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-06 20:22:54 +0900

    Title: 차이가 가장 작은 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240206/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
+++ 240206/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <set>
 #include <algorithm>
-#include <climits>
 
 using namespace std;
 
@@ -18,22 +17,35 @@ int main() {
 		s.insert(arr[i]);
 	}
 
+	// 각 숫자 x 대해
+	// x보다 m 이상 더 크면서 가장 작은 값과
+	// x보다 m 이상 더 작으면서 가장 큰 값을 구해
+	// 차이가 가장 작은 경우를 갱신합니다.
 	for (int i = 0; i < n; i++) {
-		//arr[i] 보다 낮은 위치를 찾음.
-		for (auto it = next(s.lower_bound(arr[i])); it != s.end(); it++) {
-			int sub = abs(arr[i] - *it);
+		int x = arr[i];
 
-			if (sub >= m) {
-				ans = min(ans, sub);
-			}
+		// x보다 m 이상 더 크면서 가장 작은 값은
+		// r - x >= m를 만족하는 최소 r이므로
+		// r >= m + x을 만족하는 최소 r을 구하면 됩니다.
+		if (s.lower_bound(m + x) != s.end()) {
+			ans = min(ans, *s.lower_bound(m + x) - x);
+		}
+
+	   // x보다 m 이상 더 작으면서 가장 큰 값은
+	   // x - r >= m를 만족하는 최대 r이므로
+	   // r <= x - m을 만족하는 최대 r을 구하면 됩니다.
+		set<int>::iterator it = s.upper_bound(x - m);
+		if (it != s.begin()) {
+			it--;
+			ans = min(ans, x - *it);
 		}
 
 	}
 
-    if(ans == INT_MAX) {
-        cout << -1;
-    }
-    else {
-        cout << ans;
-    }
+	if (ans == INT_MAX) {
+		cout << -1;
+	}
+	else {
+		cout << ans;
+	}
 }
