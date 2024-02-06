71f53418a5eff24a287365510fad1d02624a950c
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-06 20:01:40 +0900

    Title: 자리 차지하기, Time: 143ms, Memory: 5MB, Status: Passed - CodeTree

--- 240206/자리 차지하기/take-place.cpp
+++ 240206/자리 차지하기/take-place.cpp
@@ -4,44 +4,38 @@
 using namespace std;
 
 int n, m;
+int arr[100001];
 set<int> s;
-int cnt = 0;
 
 int main() {
 	cin >> n >> m;
 
-	if (n == 1) {
-		cout << 1;
-		return 0;
+	for(int i = 0; i < n ;i++) {
+		cin >> arr[i];
 	}
 
 	for (int i = 1; i <= m; i++) {
 		s.insert(i);
 	}
 
+	int ans = 0;
 	for (int i = 0; i < n; i++) {
-		bool flag = false;
-		int a;
-		cin >> a;
+		//ai 보다 큰 위치를 먼저 찾음.
+		set<int>::iterator it = s.upper_bound(arr[i]);
 
+		//만약 큰 최초의 위치가 첫 번째 위치가 아니라면,
+		//바로 전위치가 ai 보다 같거나 작은 최대 위치가 구해지므로,
+		//해당 위치에 사람을 앉혀줌.
+		if (it != s.begin()) {
+			it--;
+			s.erase(*it);
 
-		for (int j = a; j >= 1; j--) {
-			if (s.count(j) == 1) {
-				s.erase(j);
-				flag = true;
-				cnt++;
-				break;
-				
-			}
-			else {
-				continue;
-			}
+			ans++;
 		}
-
-		if (flag == false) {
+		else {
 			break;
 		}
 	}
 
-	cout << cnt;
+	cout << ans;
 }
