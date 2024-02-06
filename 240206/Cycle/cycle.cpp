89c9b0250c0243e3044cb407e592421aeba65a1d
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-06 15:16:03 +0900

    Title: Cycle, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/Cycle/cycle.cpp
+++ 240206/Cycle/cycle.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int n, p, num = 1, cnt = 0;
+vector<int> v;
+int n, p, num = 1, cnt = 0, ans = 0;
+
+bool Simul() {
+	for (int i = 0; i < v.size(); i++) {
+		for (int j = i + 1; j < v.size(); j++) {
+			if (v[i] == v[j]) {
+				ans = j - i;
+				return true;
+			}
+		}
+	}
+	return false;
+}
 
 int main() {
 	cin >> n >> p;
@@ -11,13 +25,18 @@ int main() {
 	while (1) {
 		num *= n;
 
-		cnt++;
-		if (num == n) {
+		num %= p;
+		
+		v.push_back(num);
+
+		if (Simul()) {
 			break;
 		}
-
-		num %= p;
+		else {
+			continue;
+		}
 	}
 
-	cout << cnt;
+	cout << ans;
+
 }
