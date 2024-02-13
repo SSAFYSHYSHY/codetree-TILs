48a57a617d9650dbd855f4825276155c2a352194
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-13 13:41:24 +0900

    Title: 자연수 n개의 합, Time: 1006ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240213/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+long long s;
+
+int main() {
+	cin >> s;
+	long long sum = 0;
+	long long num = 0;
+
+	while (sum < s) {
+		num++;
+		sum += num;
+	}
+	if(sum>s)
+		cout << num - 1;
+	else
+		cout << num;
+
+	return 0;
+}
