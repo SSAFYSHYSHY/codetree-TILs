61aada25ca7e1f7ffba9929f56621b2e35993d72
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-14 00:48:55 +0900

    Title: 삼 오 무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240214/삼 오 무/three-five-moo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+long long n;
+
+long long Calc(long long mid) {
+	return mid - (mid / 3) - (mid / 5 )+ (mid / 15);
+}
+
+int main() {
+	cin >> n;
+
+	long long left = 1;
+	long long right = 1000000000;
+	long long min_num = 1000000000;
+
+	while (left <= right) {
+		long long mid = (left + right) / 2;
+
+		if (Calc(mid) >= n) {
+			right = mid - 1;
+			min_num = min(min_num, mid);
+		}
+		else if(Calc(mid) < n){
+			left = mid + 1;
+		}
+	}
+
+	cout << min_num;
+
+}
