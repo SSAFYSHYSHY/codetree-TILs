167796aa9adbe1963df4fec07d420c867bf46a87
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-18 12:39:50 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 171ms, Memory: 1MB, Status: Passed - CodeTree

--- 240218/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.cpp
+++ 240218/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.cpp
@@ -1,54 +1,59 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <tuple>
+
+#define MAX_M 100000
+#define MAX_NUM (long long)(1e18)
 
 using namespace std;
 
 int n, m;
-vector<int> a;
+pair<long long, long long> segments[MAX_M];
 
-bool is_possible(int dist) {
-    int cnt = 1;
-    int last = a[0];
-    for (int i = 1; i < a.size(); ++i) {
-        if (a[i] - last >= dist) {
-            last = a[i];
-            cnt += 1;
+bool IsPossible(long long dist) {
+
+    int cnt = 0;
+    long long last_x = -MAX_NUM;
+    for(int i = 0; i < m; i++) {
+        long long a, b;
+        tie(a, b) = segments[i];
+        
+        while(last_x + dist <= b) {
+            cnt++;
+            last_x = max(a, last_x + dist);
+            if(cnt >= n)
+                break;
         }
     }
     return cnt >= n;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
+  
     cin >> n >> m;
-    for (int i = 0; i < m; ++i) {
-        int start, end;
-        cin >> start >> end;
-        for (int elem = start; elem <= end; ++elem) {
-            a.push_back(elem);
-        }
+    for(int i = 0; i < m; i++) {
+        long long a, b;
+        cin >> a >> b;
+        segments[i] = make_pair(a, b);
     }
-    
-    sort(a.begin(), a.end());
-    
-    int left = 1;
-    int right = 1000000000;
-    int ans = -1;
-    
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (is_possible(mid)) {
-            left = mid + 1;
-            ans = mid;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    cout << ans << endl;
 
+  
+    sort(segments, segments + m);
+    
+    long long left = 1;                         
+    long long right = MAX_NUM;                 
+    long long ans = 0;                        
+    
+    while (left <= right) {                     
+        long long mid = (left + right) / 2;     
+        if(IsPossible(mid)) {                   
+            left = mid + 1;                     
+            ans = max(ans, mid);               
+        }
+        else                               
+            right = mid - 1;                    
+    }
+
+    cout << ans;
     return 0;
 }
