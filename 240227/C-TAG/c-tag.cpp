5f69fce361a0719167c93ded9db61d30d2f1f4bb
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-02-27 12:51:33 +0900

    Title: C-TAG, Time: 1177ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/C-TAG/c-tag.cpp
+++ 240227/C-TAG/c-tag.cpp
@@ -1,66 +1,43 @@
 #include <iostream>
 #include <unordered_set>
-#include <string>
-
 using namespace std;
 
-int n, m, offset[3], cnt;
-string a[1005], b[1005];
+#define MAX_N 500
 
-void backtracking(int curr, int last) {
-    if (curr == 3) {
+int n, m;
+int ans;
+string A[MAX_N], B[MAX_N];
+unordered_set<string> s;
 
-        unordered_set<string> all;
+bool TestLocation(int x, int y, int z) {
+    s.clear();
 
-        unordered_set<string> arr;
-        for (int j = 0; j < n; j++) {
-            string temp = "";
-            for (int i = 0; i < 3; i++) {
-                temp += a[j][offset[i]];
-            }
-            arr.insert(temp);
-            all.insert(temp);
-        }
-
-
-        unordered_set<string> arr2;
-        for (int j = 0; j < n; j++) {
-            string temp = "";
-            for (int i = 0; i < 3; i++) {
-                temp += b[j][offset[i]];
-            }
-            arr2.insert(temp);
-            all.insert(temp);
-        }
-
-        if (arr.size() + arr2.size() == all.size()) 
-            cnt++;
-
-        return;
+    for(int i = 0; i < n; i++) {
+        s.insert(A[i].substr(x, 1) + A[i].substr(y, 1) + A[i].substr(z, 1));
     }
 
-    for (int i = last + 1; i < m; i++) {
-        offset[curr] = i;
-        backtracking(curr + 1, i);
+    for(int i = 0; i < n; i++) {
+        if(s.find(B[i].substr(x, 1) + B[i].substr(y, 1) + B[i].substr(z, 1)) != s.end())
+            return false;
     }
 
+    return true;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) 
-        cin >> a[i];
-    for (int i = 0; i < n; i++) 
-        cin >> b[i];
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+    
+    for(int i = 0; i < n; i++)
+        cin >> B[i];
 
-    backtracking(0, -1);
-
-    cout << cnt;
-
+    for (int i = 0; i < m; i++)
+        for (int j = i + 1; j < m; j++)
+            for (int k = j + 1; k < m; k++)
+                if (TestLocation(i, j, k)) ans++;
+    
+    cout << ans;
     return 0;
 }
