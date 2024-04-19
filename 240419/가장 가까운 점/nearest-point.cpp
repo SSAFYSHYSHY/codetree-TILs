fa39150c9482964957f7a2ef331f1cd186a87f8e
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-04-19 14:06:26 +0900

    Title: 가장 가까운 점, Time: 91ms, Memory: 3MB, Status: Passed - CodeTree

--- 240419/가장 가까운 점/nearest-point.cpp
+++ 240419/가장 가까운 점/nearest-point.cpp
@@ -19,21 +19,24 @@ int main() {
 		v.push_back(make_pair(x, y));
 	}
 
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < n; i++) {
 		int x = v[i].first;
 		int y = v[i].second;
-		q.push(make_tuple(-(abs(x) + abs(y)), -x, -y));
+		q.push(make_tuple(-(x + y), -x, -y));
+	}
 
-		tuple<int, int, int> best = q.top();
+	while (m--) {
+		int x, y;
+		tie(ignore, x, y) = q.top();
 		q.pop();
 
-		int px, py;
-		tie(ignore, px, py) = best;
+		x = -x;
+		y = -y;
 
-		px -= 2;
-		py -= 2;
+		x += 2;
+		y += 2;
 
-		q.push(make_tuple(-(abs(px) + abs(py)), px, py));
+		q.push(make_tuple(-(x + y), -x, -y));
 	}
 
 	int ax, ay;
