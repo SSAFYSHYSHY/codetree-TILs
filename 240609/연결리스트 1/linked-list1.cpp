18f4f1ca5f126bcd1465087194c7c846928d66d7
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-06-09 13:27:10 +0900

    Title: 연결리스트 1, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240609/연결리스트 1/linked-list1.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s, S;
+int N;
+
+struct Node {
+	string data;
+	Node* prev, * next;
+	Node(string data) : data(data), prev(nullptr), next(nullptr) {}
+};
+
+void Insert_Front(Node* u, Node* new_u) {
+	new_u->prev = u->prev;
+	new_u->next = u;
+
+	if (nullptr != new_u->prev) {
+		new_u->prev->next = new_u;
+	}
+	if (nullptr != new_u->next) {
+		new_u->next->prev = new_u;
+	}
+}
+
+void Insert_Back(Node* u, Node* new_u) {
+	new_u->prev = u;
+	new_u->next = u->next;
+
+	if (nullptr != new_u->prev) {
+		new_u->prev->next = new_u;
+	}
+	if (nullptr != new_u->next) {
+		new_u->next->prev = new_u;
+	}
+}
+
+void Pop_front(Node*& u) {
+	if (nullptr != u->prev) {
+		u = u->prev;
+	}
+}
+
+void Pop_back(Node*& u) {
+	if (nullptr != u->next) {
+		u->next = u;
+	}
+}
+
+int main() {
+	cin >> S >> N;
+
+	Node* node = new Node(S);
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+
+		if (a == 1) {
+			cin >> s;
+			Node* new_node = new Node(s);
+
+			Insert_Front(node, new_node);
+		}
+
+		else if (a == 2) {
+			cin >> s;
+			Node* new_node = new Node(s);
+
+			Insert_Back(node, new_node);
+		}
+
+		else if (a == 3) {
+			Pop_front(node);
+		}
+		else if (a == 4) {
+			Pop_back(node);
+		}
+
+		if (nullptr == node->prev && nullptr != node->next) {
+			cout << "(Null) " << node->data << " " << node->next->data << "\n";
+		}
+		else if (nullptr != node->prev && nullptr == node->next) {
+			cout << node->prev->data << " " << node->data << " (Null)\n";
+		}
+		else if (nullptr == node->prev && nullptr == node->next) {
+			cout << "(Null) " << node->data << " (Null)\n";
+		}
+
+		else {
+			cout << node->prev->data << " " << node->data << " " << node->next->data << "\n";
+		}
+	}
+
+
+
+}
