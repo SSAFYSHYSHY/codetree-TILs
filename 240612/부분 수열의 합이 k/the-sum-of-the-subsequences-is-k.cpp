faf4d879dd46e54b406b4701eb1821facf0ffe4e
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2024-06-12 18:19:42 +0900

    Title: 부분 수열의 합이 k, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240612/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1001];
+int prefix[1001];
+int n, k, cnt = 0, ans = 0;
+
+int Get(int s, int e) {
+	return prefix[e] - prefix[s - 1];
+}
+
+int main() {
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	prefix[0] = arr[0];
+	for (int i = 1; i < n; i++) {
+		prefix[i] = prefix[i - 1] + arr[i];
+	}
+
+	for (int i = 2; i < n; i++) {
+		for (int j = 0; j < n - i + 1; j++) {
+			ans = Get(j, j + i - 1);
+
+			if (ans == k) {
+				cnt++;
+			}
+		}
+	}
+
+	cout << cnt;
+}
