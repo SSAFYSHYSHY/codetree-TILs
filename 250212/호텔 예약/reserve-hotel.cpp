808e9dd4619d11052ebb60b067d2dce8b20c7a54
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-12 12:30:02 +0900

    Title: 호텔 예약, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250212/호텔 예약/reserve-hotel.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+using namespace std;
+
+int n;
+
+vector<pair<int, int>> points, seg;
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		seg.push_back({ x,y });
+	}
+
+	for (int i = 0; i < n; i++) {
+		int x1, x2;
+		tie(x1, x2) = seg[i];
+
+		points.push_back({ x1, +1 });
+		points.push_back({ x2, -1 });
+	}
+
+	sort(points.begin(), points.end());
+
+	int sum_val = 0, ans = 0;
+	for (int i = 0; i < 2 * n; i++) {
+		int x, v;
+		tie(x, v) = points[i];
+
+		sum_val += v;
+
+		ans = max(ans, sum_val);
+	}
+
+	cout << ans;
+}
