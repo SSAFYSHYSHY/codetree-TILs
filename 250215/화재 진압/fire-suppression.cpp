8c08ea90eb7ca21e14a986953e5fa2fea890d2d3
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-15 18:22:24 +0900

    Title: 화재 진압, Time: 75ms, Memory: 1MB, Status: Passed - CodeTree

--- 250215/화재 진압/fire-suppression.cpp
+++ 250215/화재 진압/fire-suppression.cpp
@@ -3,45 +3,34 @@
 
 using namespace std;
 
-int n, m;
-int arr[100002];
-int office[100002];
-int ans = 0;
+long long n, m;
+long long arr[100002];   // 특정 위치 배열
+long long office[100002]; // 사무실 위치 배열
+long long ans = 0;
 
 int main() {
-	cin >> n >> m;
+    cin >> n >> m;
 
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-	for (int i = 0; i < m; i++) {
-		cin >> office[i];
-	}
+    for (int i = 0; i < m; i++) {
+        cin >> office[i];
+    }
 
-	sort(arr, arr + n);
-	sort(arr, arr + m);
+    sort(arr, arr + n);
+    sort(office, office + m); // 정렬된 arr와 office 배열을 활용
 
-	int j = 0;
-	for (int i = 0; i < m; i++) {
-		while (j < n) {
-			
-			if (i < m - 1) {
-				if (abs(office[i] - arr[j]) <= abs(office[i + 1] - arr[j])) {
-					ans = max(ans, abs(office[i] - arr[j]));
-					j++;
-				}
-				else {
-					break;
-				}
-			}
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        // arr[i]에 대해 가장 가까운 사무실을 찾는다.
+        while (j + 1 < m && abs(office[j + 1] - arr[i]) <= abs(office[j] - arr[i])) {
+            j++;
+        }
+        ans = max(ans, abs(office[j] - arr[i]));
+    }
 
-			else {
-				ans = max(ans, abs(office[i] - arr[j]));
-				j++;
-			}
-		}
-	}
-
-	cout << ans;
+    cout << ans;
+    return 0;
 }
