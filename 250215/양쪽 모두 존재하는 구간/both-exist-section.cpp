35a6416fe43c6be511463234eacbe1bc78719601
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-15 18:09:54 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250215/양쪽 모두 존재하는 구간/both-exist-section.cpp
+++ 250215/양쪽 모두 존재하는 구간/both-exist-section.cpp
@@ -1,55 +1,77 @@
 #include <iostream>
-#include <unordered_map>
-#include <vector>
+#include <algorithm>
+#include <climits>
+
+#define MAX_N 100000
+#define MAX_M 100000
 
 using namespace std;
 
+// 변수 선언
 int n, m;
-vector<int> arr;
-unordered_map<int, int> inside, outside;
+int arr[MAX_N + 1];
+int count_array_in[MAX_M + 1];  // 구간 내 숫자 정보
+int count_array_out[MAX_M + 1]; // 구간 외 숫자 정보
 
-bool valid() {
-    if (inside.size() < m) return false; // 구간 안에 1~M이 하나씩 존재해야 함.
+int distinct_num_in;  // 구간 내 서로 다른 숫자 개수
+int distinct_num_out; // 구간 외 서로 다른 숫자 개수
 
-    for (int num = 1; num <= m; num++) {
-        if (outside[num] > 0) return true; // 구간 밖에도 1~M 중 하나가 존재해야 함.
-    }
-    return false;
+void Push(int idx) {
+    int num = arr[idx];
+
+    if(count_array_in[num] == 0)
+        distinct_num_in++;
+    count_array_in[num]++;
+
+    if(count_array_out[num] == 0)
+        distinct_num_out--;
+}
+
+void Pop(int idx) {
+    int num = arr[idx];
+
+    count_array_in[num]--;
+    if(count_array_in[num] == 0)
+        distinct_num_in--;
+
+    if(count_array_out[num] == 0)
+        distinct_num_out++; 
+    count_array_out[num]++;
 }
 
 int main() {
     cin >> n >> m;
-    arr.resize(n);
-
-    for (int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++)
         cin >> arr[i];
-        if (1 <= arr[i] && arr[i] <= m) {
-            outside[arr[i]]++; // 초기에 모든 숫자는 구간 밖에 존재
-        }
+
+    for(int i = 1; i <= n; i++) {
+        if(count_array_out[arr[i]] == 0)
+            distinct_num_out++;
+        
+        count_array_out[arr[i]]++;
     }
 
-    int j = 0, ans = n + 1; // 최소 길이를 찾으므로 ans를 충분히 큰 값으로 설정
-
-    for (int i = 0; i < n; i++) {
-        while (j < n && !valid()) {
-            if (1 <= arr[j] && arr[j] <= m) {
-                inside[arr[j]]++; // 구간 안으로 이동
-                outside[arr[j]]--; // 구간 밖에서 제거
-                if (outside[arr[j]] == 0) outside.erase(arr[j]);
-            }
+    int ans = INT_MAX;
+    
+    int j = 0;
+    for(int i = 1; i <= n; i++) {
+        while(j + 1 <= n && distinct_num_in < m) {
+            Push(j + 1);
             j++;
         }
 
-        if (valid()) ans = min(ans, j - i); // 조건 만족 시 최솟값 갱신
+        if(distinct_num_in < m)
+            break;
+        
+        if(distinct_num_out == m)
+            ans = min(ans, j - i + 1);
 
-        // i가 증가하면 현재 요소를 구간에서 제거
-        if (1 <= arr[i] && arr[i] <= m) {
-            inside[arr[i]]--;
-            if (inside[arr[i]] == 0) inside.erase(arr[i]);
-            outside[arr[i]]++; // 제거된 숫자는 다시 구간 밖으로
-        }
+        Pop(i);
     }
 
-    cout << (ans == n + 1 ? -1 : ans) << endl; // 답이 갱신되지 않았으면 -1 출력
+    if(ans == INT_MAX)
+        ans = -1;
+
+    cout << ans;
     return 0;
 }
