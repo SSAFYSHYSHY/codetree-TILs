df898224f1b0a734e99209a1155f2e0141663b1b
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-19 13:08:50 +0900

    Title: 폭탄 해체 작업, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

--- 250219/폭탄 해체 작업/the-bomb-dismantling.cpp
+++ 250219/폭탄 해체 작업/the-bomb-dismantling.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <tuple>
+#include <queue>
+
+#define MAX_N 10000
+#define MAX_T 10000
 
 using namespace std;
 
-struct Bomb {
-    int score, time_limit;
-};
+// 변수 선언
+int n;
+pair<int, int> bombs[MAX_N];
 
-// 정렬 기준: 시간 제한이 긴 순으로 정렬 (내림차순)
-bool cmp(const Bomb &a, const Bomb &b) {
-    return a.time_limit > b.time_limit;
+int GetTimeLimit(int bomb_idx) {
+    int time_limit;
+    tie(time_limit, ignore) = bombs[bomb_idx];
+
+    return time_limit;
 }
 
-int maxBombScore(int n, vector<Bomb>& bombs) {
-    // 제한 시간이 긴 순서로 정렬
-    sort(bombs.begin(), bombs.end(), cmp);
-
-    priority_queue<int, vector<int>, greater<int>> pq;  // 최소 힙 (점수가 낮은 것 우선 삭제)
-    int totalScore = 0;
-    int currentTime = 0;
-
-    for (const Bomb &bomb : bombs) {
-        if (currentTime < bomb.time_limit) {
-            // 아직 시간 내에 해체 가능하면 추가
-            pq.push(bomb.score);
-            totalScore += bomb.score;
-            currentTime++;
-        } else if (!pq.empty() && pq.top() < bomb.score) {
-            // 점수가 더 높은 폭탄을 해체하기 위해 낮은 점수의 폭탄 제거
-            totalScore += bomb.score - pq.top();
-            pq.pop();
-            pq.push(bomb.score);
-        }
-    }
-
-    return totalScore;
+int GetScore(int bomb_idx) {
+    int score;
+    tie(ignore, score) = bombs[bomb_idx];
+    
+    return score;
 }
 
 int main() {
-    int n;
+    // 입력:
     cin >> n;
-
-    vector<Bomb> bombs(n);
-    for (int i = 0; i < n; i++) {
-        cin >> bombs[i].score >> bombs[i].time_limit;
+    for(int i = 0; i < n; i++) {
+        int score, time_limit;
+        cin >> score >> time_limit;
+        bombs[i] = make_pair(time_limit, score);
     }
 
-    cout << maxBombScore(n, bombs) << endl;
+    sort(bombs, bombs + n);
+
+    priority_queue<int> pq;
+    int bomb_idx = n - 1;
+    int ans = 0;
+
+    for(int t = MAX_T; t >= 1; t--) {
+        while(bomb_idx >= 0 && GetTimeLimit(bomb_idx) >= t) {
+            pq.push(GetScore(bomb_idx));
+            bomb_idx--;
+        }
+
+        if(!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+    }
+
+    cout << ans;
     return 0;
 }
