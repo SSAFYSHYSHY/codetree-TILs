43139d93ddf1230fcf47cc33034c98355abbe549
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-19 11:54:39 +0900

    Title: 수 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250219/수 채우기/fill-in-number.cpp
+++ 250219/수 채우기/fill-in-number.cpp
@@ -2,18 +2,28 @@
 
 using namespace std;
 
-int n;
-int arr[2] = { 5,2 };
-
 int main() {
-	cin >> n;
+    int n;
+    cin >> n;
 
-	int cnt = 0;
-	for (int i = 0; i < 2; i++) {
-		cnt += (n / arr[i]);
-		n %= arr[i];
-	}
+    if (n < 5 && n % 2 != 0) {
+        cout << -1;
+        return 0;
+    }
 
-	cout << cnt;
+    int a = n / 5;
+    int b = n % 5;
 
+    while (b % 2 != 0 && a > 0) {  // 2원으로 해결할 수 없으면 5원을 줄여본다
+        a--;
+        b += 5;
+    }
+
+    if (b % 2 == 0) {
+        cout << a + (b / 2);
+    } else {
+        cout << -1;
+    }
+
+    return 0;
 }
