c4716a972c3a4d1f1694d5912ea53f09c85aea1d
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-20 15:11:31 +0900

    Title: 좌우 반전시키기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250220/좌우 반전시키기/flip-left-and-right.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int count = 0;
+    
+    // 2번째 칸부터 순차적으로 진행
+    for (int i = 1; i < n; i++) {
+        if (arr[i - 1] == 0) {
+            // i 번째 칸을 눌러야 함
+            count++;
+            arr[i - 1] ^= 1;
+            arr[i] ^= 1;
+            if (i + 1 < n) {
+                arr[i + 1] ^= 1;
+            }
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
