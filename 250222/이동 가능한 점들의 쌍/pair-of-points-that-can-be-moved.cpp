a0a61472078cde7472a7e200490f7dd85e212910
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-22 12:53:59 +0900

    Title: 이동 가능한 점들의 쌍, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250222/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.cpp
+++ 250222/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.cpp
@@ -3,17 +3,17 @@
 #include <algorithm>
 
 using namespace std;
-const int INF = 1e9;  // 무한대 값 설정
+const long long INF = 1e9;  // 무한대 값 설정
 
-int N, M, P, Q;
-vector<int> redPoints;
-vector<vector<int>> dist;
+long long N, M, P, Q;
+vector<long long> redPoint;
+vector<vector<long long>> dist;
 
-bool hasRedPoint(int A, int B) {
+bool hasRedPoint(long long A, long long B) {
     if (A <= P || B <= P) return true; // 출발점 또는 도착점이 빨간점인 경우
 
     // 경로 내 빨간점이 포함되는지 확인
-    for (int red : redPoints) {
+    for (long long red : redPoint) {
         if (dist[A][red] != INF && dist[red][B] != INF) {
             return true;
         }
@@ -28,25 +28,25 @@ int main() {
     cin >> N >> M >> P >> Q;
     
     // 거리 행렬 초기화
-    dist.assign(N + 1, vector<int>(N + 1, INF));
-    for (int i = 1; i <= N; i++) dist[i][i] = 0;
+    dist.assign(N + 1, vector<long long>(N + 1, INF));
+    for (long long i = 1; i <= N; i++) dist[i][i] = 0;
 
     // 빨간점 리스트 저장
-    for (int i = 1; i <= P; i++) {
-        redPoints.push_back(i);
+    for (long long i = 1; i <= P; i++) {
+        redPoint.push_back(i);
     }
 
     // 간선 입력
-    for (int i = 0; i < M; i++) {
-        int u, v, w;
+    for (long long i = 0; i < M; i++) {
+        long long u, v, w;
         cin >> u >> v >> w;
         dist[u][v] = w;
     }
 
     // Floyd-Warshall 알고리즘 수행
-    for (int k = 1; k <= N; k++) {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+    for (long long k = 1; k <= N; k++) {
+        for (long long i = 1; i <= N; i++) {
+            for (long long j = 1; j <= N; j++) {
                 if (dist[i][k] != INF && dist[k][j] != INF) {
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
@@ -54,12 +54,12 @@ int main() {
         }
     }
 
-    int validQueries = 0;
-    int totalCost = 0;
+    long long validQueries = 0;
+    long long totalCost = 0;
 
     // 질의 처리
-    for (int i = 0; i < Q; i++) {
-        int A, B;
+    for (long long i = 0; i < Q; i++) {
+        long long A, B;
         cin >> A >> B;
 
         if (dist[A][B] != INF && hasRedPoint(A, B)) {
