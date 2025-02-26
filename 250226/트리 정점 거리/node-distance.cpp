333dc762e49e1a6bce5882330625cbd1fd9c656d
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-02-26 17:56:07 +0900

    Title: 트리 정점 거리, Time: 21ms, Memory: 0MB, Status: Passed - CodeTree

--- 250226/트리 정점 거리/node-distance.cpp
+++ 250226/트리 정점 거리/node-distance.cpp
@@ -1,70 +1,62 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#define MAX_N 1000
+#include <cstring> // memset 사용
+#define MAX_N 1001
 
 using namespace std;
 
 int n, m;
-vector<pair<int, int>> v[1001];
-bool visited[1001];
-int dist[1001];
+vector<pair<int, int>> v[MAX_N];
+bool visited[MAX_N];
+int dist[MAX_N];
 
 void Input() {
-	cin >> n >> m;
+    cin >> n >> m;
 
-	for (int i = 0; i < n - 1; i++) {
-		int x, y, w;
-		cin >> x >> y >> w;
+    for (int i = 0; i < n - 1; i++) {
+        int x, y, w;
+        cin >> x >> y >> w;
 
-		v[x].push_back({ y,w });
-		v[y].push_back({ x,w });
-	}
+        v[x].push_back({ y, w });
+        v[y].push_back({ x, w });
+    }
 }
 
 void DFS(int start, int endi) {
-	//만약 원하는 정점에 도달하면.
-	if (start == endi) {
-		return;
-	}
+    if (start == endi) return;
 
-	//그렇지 않으면 계속 DFS 색출.
-	for (int i = 0; i < v[start].size(); i++) {
-		int y = v[start][i].first;
-		int cost = v[start][i].second;
+    for (auto &next : v[start]) {
+        int y = next.first;
+        int cost = next.second;
 
-		if (!visited[y]) {
-			visited[y] = true;
-			dist[y] = dist[start] + cost;
-			DFS(y, endi);
-		}
-	}
+        if (!visited[y]) {
+            visited[y] = true;
+            dist[y] = dist[start] + cost;
+            DFS(y, endi);
+        }
+    }
 }
 
 int Calc(int start, int endi) {
-	//visited 초기화 및 start 위치에 대해 true 연산.
-	for (int i = 0; i < MAX_N; i++) {
-		visited[i] = false;
-		dist[i] = 0;
-	}
-	visited[start] = true;
-	dist[start] = 0;
+    memset(visited, false, sizeof(visited));
+    memset(dist, 0, sizeof(dist));
 
-	DFS(start, endi);
+    visited[start] = true;
+    dist[start] = 0;
+    DFS(start, endi);
 
-	return dist[endi];
+    return dist[endi];
 }
 
 int main() {
-	Input();
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-	//m 만큼 반복해서 시뮬 시작.
-	for (int i = 0; i < m; i++) {
-		int s, e;
-		cin >> s >> e;
+    Input();
 
-		int dist = Calc(s, e);
-
-		cout << dist << "\n";
-	}
+    for (int i = 0; i < m; i++) {
+        int s, e;
+        cin >> s >> e;
+        cout << Calc(s, e) << "\n";
+    }
 }
