8727003fc10d56b628bd0c8bacbe6b9d899dd081
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-03-19 18:21:00 +0900

    Title: 바이러스 실험, Time: 811ms, Memory: 2MB, Status: Passed - CodeTree

--- 250319/바이러스 실험/virus-experiment.cpp
+++ 250319/바이러스 실험/virus-experiment.cpp
@@ -77,33 +77,6 @@ void Calc1() {
 	v = alive;
 }
 
-void Calc2() {
-	for (int i = 0; i < v.size(); i++) {
-		int x = v[i].x;
-		int y = v[i].y;
-		int age = v[i].age;
-		bool flag = v[i].live;
-
-		//못 먹어 죽은 바이러스라면
-		//나이를 나눠 arr에 누적.
-		if (!flag) {
-			arr[x][y] = arr[x][y] + (age / 2);
-		}
-		//그렇지 않으면 temp_v에 누적해서 v 에 정렬하기 위함.
-		else {
-			temp_v.push_back({ x,y,age,true });
-		}
-	}
-
-	v.clear();
-
-	for (int i = 0; i < temp_v.size(); i++) {
-		if (temp_v[i].live == true) {
-			v.push_back({ temp_v[i] });
-		}
-	}
-}
-
 void Calc3() {
 	vector<Node> v2;
 
@@ -137,35 +110,17 @@ void Calc4() {
 	}
 }
 
-void Print() {
-	cout << "\n\n";
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cout << arr[i][j] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n\n";
-}
-
-void Print2() {
-	cout << "\n\n";
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i].x << " " << v[i].y << " " << v[i].age << "\n";
-	}
-	cout << "\n\n";
-}
-
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
 	Input();
 
 	for (int i = 0; i < k; i++) {
-		//바이러스의 크기 만큼 진행.
+		//바이러스의 크기 만큼 진행, 죽은거 처리.
 		//우선 정렬.
 		Calc1();
-
-		//죽은 바이러스 처리.
-		//Calc2();
 
 		//8 방향 순회.
 		Calc3();
