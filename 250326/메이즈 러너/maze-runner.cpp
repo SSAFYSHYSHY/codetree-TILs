50a2c47c24c8a5cd952ae1cac9cac7a0aa250c42
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-03-26 23:12:56 +0900

    Title: 메이즈 러너, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250326/메이즈 러너/maze-runner.cpp
+++ 250326/메이즈 러너/maze-runner.cpp
@@ -1,236 +1,198 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
-struct Node {
-	int x, y;
-	int dist;
-	bool flag;
-};
+#define MAX_N 10
 
 int n, m, k;
-int arr[21][21];
-int new_arr[21][21];
-vector<Node> v;
+
+int board[MAX_N + 1][MAX_N + 1];
+
+int next_board[MAX_N + 1][MAX_N + 1];
+
+
+pair<int, int> traveler[MAX_N + 1];
+
 pair<int, int> exits;
 
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
 
-void Input(){
-	cin >> n >> m >> k;
+int ans;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> arr[i][j];
-		}
-	}
 
-	for (int i = 0; i < m; i++) {
-		int x, y;
-		cin >> x >> y;
+int sx, sy, square_size;
 
-		x--;
-		y--;
 
-		v.push_back({ x,y,0,false });
-	}
+void MoveAllTraveler() {
 
-	int x, y;
-	cin >> x >> y;
-	x--; y--;
-	exits.first = x;
-	exits.second = y;
+    for(int i = 1; i <= m; i++) {
+
+        if(traveler[i].first == exits.first && traveler[i].second == exits.second)
+            continue;
+        
+
+        if(traveler[i].first != exits.first) {
+            int nx = traveler[i].first;
+            int ny = traveler[i].second;
+
+            if(exits.first > nx) nx++;
+            else nx--;
+
+            if(!board[nx][ny]) {
+                traveler[i].first = nx;
+                traveler[i].second = ny;
+                ans++;
+                continue;
+            }
+        }
+
+
+        if(traveler[i].second != exits.second) {
+            int nx = traveler[i].first;
+            int ny = traveler[i].second;
+
+            if(exits.second > ny) ny++;
+            else ny--;
+
+            if(!board[nx][ny]) {
+                traveler[i].first = nx;
+                traveler[i].second = ny;
+                ans++;
+                continue;
+            }
+        }
+    }
 }
 
-bool InRange(int x, int y) {
-	return 0 <= x && x < n && 0 <= y && y < n;
+
+void FindMinimumSquare() {
+
+    for(int sz = 2; sz <= n; sz++) {
+
+        for(int x1 = 1; x1 <= n - sz + 1; x1++) {
+
+            for(int y1 = 1; y1 <= n - sz + 1; y1++) {
+                int x2 = x1 + sz - 1;
+                int y2 = y1 + sz - 1;
+
+                if(!(x1 <= exits.first && exits.first <= x2 && y1 <= exits.second && exits.second <= y2)) {
+                    continue;
+                }
+
+                bool is_traveler_in = false;
+                for(int l = 1; l <= m; l++) {
+                    if(x1 <= traveler[l].first && traveler[l].first <= x2 && y1 <= traveler[l].second && traveler[l].second <= y2) {
+                        if(!(traveler[l].first == exits.first && traveler[l].second == exits.second)) {
+                            is_traveler_in = true;
+                        }
+                    }
+                }
+
+                if(is_traveler_in) {
+                    sx = x1;
+                    sy = y1;
+                    square_size = sz;
+
+                    return;
+                }
+            }
+        }
+    }
 }
 
-void Move() {
-	for (int i = 0; i < v.size(); i++) {
-		if (v[i].flag == true) continue;
 
-		int cx = v[i].x;
-		int cy = v[i].y;
-		int cdist = abs(exits.first - cx) + abs(exits.second - cy);
-		int dist = v[i].dist;
-		bool cflag = v[i].flag;
+void RotateSquare() {
 
-		int cnt = 0;
-		int ax = 0 , ay = 0;
-		for (int j = 0; j < 4; j++) {
-			int nx = cx + dx[j];
-			int ny = cy + dy[j];
+    for(int x = sx; x < sx + square_size; x++)
+        for(int y = sy; y < sy + square_size; y++) {
+            if(board[x][y]) board[x][y]--;
+        }
 
-			//가고자 하는 위치가 종료 지점이면 true로 바꾸고, dist 증가후 break
-			if (nx == exits.first && ny == exits.second) {
-				cflag = true;
-				cnt++;
-				break;
-			}
 
-			//아니면 이동한 곳에 대해 범위 안에 들어오고 벽이 아니면.
-			if (InRange(nx, ny) && arr[nx][ny] == 0) {
-				//거리 확인
-				int ndist = abs(nx - exits.first) + abs(ny - exits.second);
+    for(int x = sx; x < sx + square_size; x++)
+        for(int y = sy; y < sy + square_size; y++) {
 
-				//거리가 더 작은 것에 대해 갱신.
-				if (ndist < cdist) {
-					cdist = ndist;
-					ax = nx;
-					ay = ny;
-					cnt++;
-				}
-			}
+            int ox = x - sx, oy = y - sy;
 
-		}
+            int rx = oy, ry = square_size - ox - 1;
 
-		//하나라도 cnt 이상을 만족하면. 해당 플레이어 재갱신
-		if (cnt > 0) {
-			v[i] = { ax,ay,dist + 1, cflag };
-		}
-		else {
-			v[i] = { cx, cy, dist, cflag };
-		}
-	}
+            next_board[rx + sx][ry + sy] = board[x][y];
+        }
+
+
+    for(int x = sx; x < sx + square_size; x++)
+        for(int y = sy; y < sy + square_size; y++) {
+            board[x][y] = next_board[x][y];
+        }
 }
 
-bool Finded(int cx, int cy, int k) {
-	bool flag1 = false, flag2 = false;
 
-	for (int i = cx; i < cx + k; i++) {
-		for (int j = cy; j < cy + k; j++) {
-			if (!InRange(i, j)) return false;
+void RotateTravelerAndExit() {
 
-			if (exits.first == i && exits.second == j) flag1 = true;
+    for(int i = 1; i <= m; i++) {
+        int x = traveler[i].first;
+        int y = traveler[i].second;
 
-			for (int l = 0; l < v.size(); l++) {
-				if (v[l].flag == true) continue;
-				if (v[l].x == i && v[l].y == j) {
-					flag2 = true;
-				}
-			}
-		}
-	}
+        if(sx <= x && x < sx + square_size && sy <= y && y < sy + square_size) {
 
-	if (flag1 == true && flag2 == true) {
-		return true;
-	}
-	return false;
-}
+            int ox = x - sx, oy = y - sy;
 
-tuple<int, int, int> Find() {
-	for (int k = 2; k <= n; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (Finded(i, j, k)) {
-					return make_tuple(i, j, k);
-				}
-			}
-		}
-	}
+            int rx = oy, ry = square_size - ox - 1;
+
+            traveler[i] = make_pair(rx + sx, ry + sy);
+        }
+    }
 
 
-}
+    int x = exits.first;
+    int y = exits.second;
+    if(sx <= x && x < sx + square_size && sy <= y && y < sy + square_size) {
 
-bool Flag() {
-	for (int i = 0; i < v.size(); i++) {
-		if (!v[i].flag) {
-			return false;
-		}
-	}
+        int ox = x - sx, oy = y - sy;
 
-	return true;
-}
+        int rx = oy, ry = square_size - ox - 1;
 
-void Rotate(int x, int y, int dice) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			new_arr[i][j] = 0;
-		}
-	}
-
-	int ex = exits.first, ey = exits.second;
-	vector<pair<int, int>> temp;
-
-	for (int i = 0; i < v.size(); i++) {
-		temp.push_back({ -1, -1 });
-	}
-
-	for (int i = 0; i < dice; i++) {
-		for (int j = 0; j < dice; j++) {
-			//출구 회전.
-			if (exits.first == x + i && exits.second == y + j) {
-				ex = x + j;
-				ey = y + dice - i - 1;
-			}
-
-			//플레이어 회전.
-			for (int k = 0; k < v.size(); k++) {
-				if (v[k].x == x + i && v[k].y == y + j) {
-					int nx = x + j;
-					int ny = y + dice - i - 1;
-					temp[k] = { nx,ny };
-				}
-			}
-
-			new_arr[i + x][y + j] = arr[x + dice - j - 1][y + i];
-		}
-	}
-
-	//출구.
-	exits = { ex,ey };
-
-	//플레이어 회전.
-	for (int i = 0; i < v.size(); i++) {
-		if (temp[i].first != -1 && temp[i].second != -1) {
-			v[i].x = temp[i].first;
-			v[i].y = temp[i].second;
-		}
-	}
-
-	//다시 옮기기.
-	for (int i = 0; i < dice; i++) {
-		for (int j = 0; j < dice; j++) {
-			arr[x + i][y + j] = new_arr[x + i][y + j];
-
-			if (arr[x + i][y + j] > 0) arr[x + i][y + j]--;
-		}
-	}
+        exits = make_pair(rx + sx, ry + sy);
+    }
 }
 
 int main() {
-	Input();
+    cin >> n >> m >> k;
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            cin >> board[i][j];
+    
+    for(int i = 1; i <= m; i++) {
+        cin >> traveler[i].first;
+        cin >> traveler[i].second;
+    }
 
-	for (int i = 0; i < 8; i++) {
+    cin >> exits.first;
+    cin >> exits.second;
 
-		//다 통과 했는지 체크.
-		if (Flag()) break;
+    while(k--) {
 
-		//플레이어들 이동.
-		Move();
+        MoveAllTraveler();
 
-		//Print();
 
-		//완탐으로 정사각형 찾기.
-		int cx, cy, dice;
-		tie(cx, cy, dice) = Find();
+        bool is_all_escaped = true;
+        for(int i = 1; i <= m; i++) {
+            if(!(traveler[i].first == exits.first && traveler[i].second == exits.second)) {
+                is_all_escaped = false;
+            }
+        }
 
-		//찾은 좌표에 대한 회전.
-		Rotate(cx, cy, dice);
 
-		//Print();
-	}
+        if(is_all_escaped) break;
 
-	int ans = 0;
-	for (int i = 0; i < v.size(); i++) {
-		ans += v[i].dist;
-	}
 
-	cout << ans << "\n";
-	cout << exits.first + 1 << " " << exits.second + 1;
+        FindMinimumSquare();
 
+
+        RotateSquare();
+
+        RotateTravelerAndExit();
+    }
+
+    cout << ans << "\n";
+    cout << exits.first << " " << exits.second;
 }
