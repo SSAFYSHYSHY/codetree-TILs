5e66944da47ea622440cc464a3f1ffd1d8327ad4
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-03-26 16:14:46 +0900

    Title: 술래잡기 체스, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250326/술래잡기 체스/odd-chess2.cpp
+++ 250326/술래잡기 체스/odd-chess2.cpp
@@ -1,146 +1,130 @@
 #include <iostream>
+#include <tuple>
 #include <algorithm>
+
+#define DIR_NUM 8
+#define MAX_N 4
+#define TAGGER make_pair(-2, -2)
+#define BLANK make_pair(-1, -1)
 
 using namespace std;
 
-int dx[] = { -1,-1,0,1,1,1,0,-1 };
-int dy[] = { 0,-1,-1,-1,0,1,1,1 };
-int s_x = 0, s_y = 0, dir = 0, ans = 0, score = 0;
-int p, d;
+int n = 4;
 
-pair<int, int> arr[11][11];
+pair<int, int> board[MAX_N][MAX_N];
 
-void Input() {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			int num, dir;
-			cin >> num >> dir;
+int dx[DIR_NUM] = { -1, -1,  0,  1, 1, 1, 0, -1 };
+int dy[DIR_NUM] = { 0, -1, -1, -1, 0, 1, 1,  1 };
 
-			arr[i][j] = { num,dir - 1 };
-		}
-	}
-	
-	//초기 점수, 방향에 대한 길.
-	score = arr[s_x][s_y].first;
-	dir = arr[s_x][s_y].second;
-	arr[s_x][s_y] = { -2,-2 };
-}
+int max_score;
 
 bool InRange(int x, int y) {
-	return 0 <= x && x < 4 && 0 <= y && y < 4;
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-bool OutRange(int x, int y, int dir) {
-	for (int i = 1; i <= 4; i++) {
-		int nx = x + dx[dir] * i;
-		int ny = y + dy[dir] * i;
-
-		if (InRange(nx, ny) && arr[x][y] != make_pair(-1,-1)) {
-			return false;
-		}
-	}
-
-	return true;
+bool ThiefCanGo(int x, int y) {
+    return InRange(x, y) && board[x][y] != TAGGER;
 }
 
-tuple<int, int, int> Get(int x, int y, int move_dir) {
-	for (int i = 0; i < 8; i++) {
-		int adj = (move_dir + i) % 8;
-
-		int nx = x + dx[adj];
-		int ny = y + dy[adj];
-
-		if (InRange(nx, ny) && arr[x][y] != make_pair(-2, -2)) {
-			return make_tuple(nx, ny, adj);
-		}
-	}
-
-	return make_tuple(x, y, move_dir);
+bool TaggerCanGo(int x, int y) {
+    return InRange(x, y) && board[x][y] != BLANK;
 }
 
-void Swap(int x, int y, int nx, int ny) {
-	pair<int, int> temp_p = arr[x][y];
+bool Done(int x, int y, int d) {
+    for (int dist = 1; dist <= n; dist++) {
+        int nx = x + dx[d] * dist, ny = y + dy[d] * dist;
+        if (TaggerCanGo(nx, ny))
+            return false;
+    }
 
-	arr[x][y] = arr[nx][ny];
-	arr[nx][ny] = temp_p;
+    return true;
 }
 
-//이동.
-void Move(int tar_num) {
-	for (int x = 0; x < 4; x++) {
-		for (int y = 0; y < 4; y++) {
-			int p_num, p_dir;
-			tie(p_num, p_dir) = arr[x][y];
+tuple<int, int, int> GetNext(int x, int y, int move_dir) {
+    for (int rotate_num = 0; rotate_num < 8; rotate_num++) {
+        int adjusted_dir = (move_dir + rotate_num) % 8;
+        int next_x = x + dx[adjusted_dir];
+        int next_y = y + dy[adjusted_dir];
+        if (ThiefCanGo(next_x, next_y))
+            return make_tuple(next_x, next_y, adjusted_dir);
+    }
+    return make_tuple(x, y, move_dir);
+}
 
-			if (p_num == tar_num) {
-				int nx, ny, ndir;
-				tie(nx, ny, ndir) = Get(x, y, p_dir);
+void Swap(int x, int y, int next_x, int next_y) {
+    pair<int, int> temp_piece = board[x][y];
+    board[x][y] = board[next_x][next_y];
+    board[next_x][next_y] = temp_piece;
+}
 
-				arr[x][y] = make_pair(p_num, ndir);
-				Swap(x, y, nx, ny);
-
-				return;
-			}
-		}
-	}
+void Move(int target_num) {
+    for (int x = 0; x < n; x++)
+        for (int y = 0; y < n; y++) {
+            int piece_num, move_dir;
+            tie(piece_num, move_dir) = board[x][y];
+            if (piece_num == target_num) {
+                int next_x, next_y, next_dir;
+                tie(next_x, next_y, next_dir) = GetNext(x, y, move_dir);
+                board[x][y] = make_pair(piece_num, next_dir);
+                Swap(x, y, next_x, next_y);
+                return;
+            }
+        }
 }
 
 void MoveAll() {
-	for (int i = 1; i <= 16; i++) {
-		Move(i);
-	}
+    for (int i = 1; i <= n * n; i++)
+        Move(i);
 }
 
-void Back(int cx, int cy, int cdir, int score) {
-	//갈 수 없으면 반환.
-	if (OutRange(cx, cy, cdir)) {
-		ans = max(ans, score);
-		return;
-	}
+void SearchMaxScore(int x, int y, int d, int score) {
+    if (Done(x, y, d)) {
+        max_score = max(max_score, score);
+        return;
+    }
 
-	//갈 수 있는 곳에 대해서 찾으면 가자.
-	int ndir = 0;
-	for (int i = 1; i <= 4; i++) {
-		int nx = cx + dx[cdir] * i;
-		int ny = cy + dy[cdir] * i;
+    for (int dist = 1; dist <= n; dist++) {
+        int nx = x + dx[d] * dist, ny = y + dy[d] * dist;
+        if (!TaggerCanGo(nx, ny))
+            continue;
 
-		if (!(InRange(nx, ny) && (arr[nx][ny] != make_pair(-1,-1)))) continue;
+        pair<int, int> temp[MAX_N][MAX_N];
 
-		pair<int, int> copy_arr[4][4];
-		for (int j = 0; j < 4; j++) {
-			for (int k = 0; k < 4; k++) {
-				copy_arr[j][k] = arr[j][k];
-			}
-		}
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                temp[i][j] = board[i][j];
 
-		//말을 잡고.
-		int next_score, next_dir;
-		tie(next_score, next_dir) = arr[nx][ny];
-		arr[nx][ny] = { -2,-2 };
-		arr[cx][cy] = { -1,-1 };
+        int extra_score, next_dir;
+        tie(extra_score, next_dir) = board[nx][ny];
+        board[nx][ny] = TAGGER;
+        board[x][y] = BLANK;
 
-		//도둑 말 이동..
-		MoveAll();
+        MoveAll();
 
-		//백트래킹 진행.
-		Back(nx, ny, next_dir, score + next_score);
+        SearchMaxScore(nx, ny, next_dir, score + extra_score);
 
-		for (int j = 0; j < 4; j++) {
-			for (int k = 0; k < 4; k++) {
-				arr[j][k] = copy_arr[j][k];
-			}
-		}
-	}
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = temp[i][j];
+    }
 }
 
 int main() {
-	Input();
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            int p, d;
+            cin >> p >> d;
+            board[i][j] = make_pair(p, d - 1);
+        }
 
-	//이동.
-	MoveAll();
+    int init_score, init_dir;
+    tie(init_score, init_dir) = board[0][0];
+    board[0][0] = TAGGER;
 
-	//백트래킹으로 가장 높은 점수를 가져감.
-	Back(s_x, s_y, dir, score);
+    MoveAll();
 
-	cout << ans;
+    SearchMaxScore(0, 0, init_dir, init_score);
+    cout << max_score;
+
+    return 0;
 }
