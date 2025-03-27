04d1c839f7ee8ae279a79f96c991b6ba993826b4
dontknowlol <109516979+SSAFYSHYSHY@users.noreply.github.com>
2025-03-27 21:14:15 +0900

    Title: 마법의 숲 탐색, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250327/마법의 숲 탐색/magical-forest-exploration.cpp
+++ 250327/마법의 숲 탐색/magical-forest-exploration.cpp
@@ -1,112 +1,101 @@
 #include <iostream>
 #include <queue>
 
+#define MAX_L 70
+
 using namespace std;
 
-int r, c, k;
-int arr[73][70];
-int dy[] = { -1,0,1,0 };
-int dx[] = { 0,1,0,-1 };
+int R, C, K;
+int A[MAX_L + 3][MAX_L]; 
+int dy[4] = { -1, 0, 1, 0 }, dx[4] = { 0, 1, 0, -1 };
+bool isExit[MAX_L + 3][MAX_L]; 
+int answer; 
 
-bool visited[73][70];
-int ans;
-
-bool InRange(int y, int x) {
-	return 3 <= y && y < r + 3 && 0 <= x && x < c;
+bool inRange(int y, int x) {
+    return 3 <= y && y < R + 3 && 0 <= x && x < C;
 }
 
-void Reset() {
-	for (int i = 0; i < r + 3; i++) {
-		for (int j = 0; j < c; j++) {
-			arr[i][j] = 0;
-			visited[i][j] = false;
-		}
-	}
+void resetMap() {
+    for (int i = 0; i < R + 3; i++) {
+        for (int j = 0; j < C; j++) {
+            A[i][j] = 0;
+            isExit[i][j] = false;
+        }
+    }
 }
 
-int BFS(int y, int x) {
-	int result = y;
-	queue<pair<int, int>> q;
-	bool visit[73][71];
 
-	for (int i = 0; i < r + 3; i++) {
-		for (int j = 0; j < c; j++) {
-			visit[i][j] = false;
-		}
-	}
-
-	q.push({ y,x });
-	visit[y][x] = true;
-
-	while (!q.empty()) {
-		pair<int, int> cur = q.front();
-		q.pop();
-
-		for (int k = 0; k < 4; k++) {
-			int ny = cur.first + dy[k];
-			int nx = cur.second + dx[k];
-
-			if (InRange(ny, nx) && !visit[ny][nx] && (arr[ny][nx] == arr[cur.first][cur.second] || (arr[ny][nx] != 0 && visited[cur.first][cur.second]))) {
-				q.push({ ny,nx });
-				visit[ny][nx] = true;
-				result = max(result, ny);
-			}
-		}
-	}
-	return result;
+bool canGo(int y, int x) {
+    bool flag = 0 <= x - 1 && x + 1 < C && y + 1 < R + 3;
+    flag = flag && (A[y - 1][x - 1] == 0);
+    flag = flag && (A[y - 1][x] == 0);
+    flag = flag && (A[y - 1][x + 1] == 0);
+    flag = flag && (A[y][x - 1] == 0);
+    flag = flag && (A[y][x] == 0);
+    flag = flag && (A[y][x + 1] == 0);
+    flag = flag && (A[y + 1][x] == 0);
+    return flag;
 }
 
-bool Go(int y, int x) {
-	bool flag = 0 <= x - 1 && x + 1 < c && y + 1 < r + 3;
+int bfs(int y, int x) {
+    int result = y;
+    queue<pair<int, int> > q;
+    bool visit[MAX_L + 3][MAX_L];
+    for (int i = 0; i < R + 3; i++) {
+        for (int j = 0; j < C; j++) {
+            visit[i][j] = false;
+        }
+    }
 
-	flag = flag && (arr[y - 1][x - 1] == 0);
-	flag = flag && (arr[y - 1][x] == 0);
-	flag = flag && (arr[y - 1][x + 1] == 0);
-	flag = flag && (arr[y][x - 1] == 0);
-	flag = flag && (arr[y][x] == 0);
-	flag = flag && (arr[y][x + 1] == 0);
-	flag = flag && (arr[y + 1][x] == 0);
-	return flag;
+    q.push(make_pair(y, x));
+    visit[y][x] = true;
+    while (!q.empty()) {
+        pair<int, int> cur = q.front();
+        q.pop();
+        for (int k = 0; k < 4; k++) {
+            int ny = cur.first + dy[k], nx = cur.second + dx[k];
+            if (inRange(ny, nx) && !visit[ny][nx] && (A[ny][nx] == A[cur.first][cur.second] || (A[ny][nx] != 0 && isExit[cur.first][cur.second]))) {
+                q.push(make_pair(ny, nx));
+                visit[ny][nx] = true;
+                result = max(result, ny);
+            }
+        }
+    }
+    return result;
 }
 
-void Down(int y, int x, int d, int id) {
-	if (Go(y + 1, x)) {
-		Down(y + 1, x, d, id);
-	}
-	else if (Go(y + 1, x - 1)) {
-		Down(y + 1, x - 1, (d + 3) % 4, id);
-	}
-	else if (Go(y + 1, x + 1)) {
-		Down(y + 1, x + 1, (d + 1) % 4, id);
-	}
-	else {
-		if (!InRange(y - 1, x - 1) || !InRange(y + 1, x + 1)) {
-			Reset();
-		}
-		else {
-			arr[y][x] = id;
-
-			for (int k = 0; k < 4; k++)
-				arr[y + dy[k]][x + dx[k]] = id;
-
-			visited[y + dy[k]][x + dx[k]] = true;
-			ans += BFS(y, x) - 3 + 1;
-		}
-	}
-
-
+void down(int y, int x, int d, int id) {
+    if (canGo(y + 1, x)) {
+        down(y + 1, x, d, id);
+    }
+    else if (canGo(y + 1, x - 1)) {
+        down(y + 1, x - 1, (d + 3) % 4, id);
+    }
+    else if (canGo(y + 1, x + 1)) {
+        down(y + 1, x + 1, (d + 1) % 4, id);
+    }
+    else {
+        if (!inRange(y - 1, x - 1) || !inRange(y + 1, x + 1)) {
+            resetMap();
+        }
+        else {
+            A[y][x] = id;
+            for (int k = 0; k < 4; k++)
+                A[y + dy[k]][x + dx[k]] = id;
+            isExit[y + dy[d]][x + dx[d]] = true;
+            answer += bfs(y, x) - 3 + 1;
+        }
+    }
 }
 
 int main() {
-	cin >> r >> c >> k;
-
-	for (int i = 1; i <= k; i++) {
-		int x, d;
-		cin >> x >> d;
-		x--;
-		Down(0, x, d, i);
-	}
-
-	cout << ans + 4;
-
+    cin >> R >> C >> K;
+    for (int id = 1; id <= K; id++) { 
+        int x, d;
+        cin >> x >> d; 
+        x--;
+        down(0, x, d, id);
+    }
+    cout << answer << endl;
+    return 0;
 }
